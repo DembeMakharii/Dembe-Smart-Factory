ab2b36568b43789c8710de81c74e01cb1edc02b7
Dembe Makhari <dembemakhari67@gmail.com>
2025-06-24 13:54:26 +0200

    chore: Connect Express app to MongoDB using Mongoose

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/smartfactory', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
